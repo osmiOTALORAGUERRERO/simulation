00e7d7c5e12d2010872ecdf4bce0a12b49fb6f22
Osmi Otalora <ootalorague@uniminuto.edu.co>
2020-10-10 17:34:35 -0500

    futures del vendedor de autos

--- montecarlo_rent_cars/car_delear.py
+++ montecarlo_rent_cars/car_delear.py
@@ -1,41 +1,71 @@
 from car import Car
+
 class CarDelear(object):
     """docstring for CarDelear."""
 
     def __init__(self, count_purchased_cars):
         super(CarDelear, self).__init__()
         self.buy_cars(count_purchased_cars)
+        self.purchased_cars = []
         self.rent_value = 350
         self.idle_value = 50
         self.busy_value = 200
 
     def buy_cars(self, count_purchased_cars):
-        pass
+        for i in range(count_purchased_cars):
+            self.purchased_cars.append(Car())
 
     #carros en ociosos o disponibles
+    @property
     def cars_idle(self):
-        pass
+        quantity = 0
+        for car in self.purchased_cars:
+            if !car.rent:
+                quantity += 1
+        return quantity
+
 
     #carros rentados o ocupados
+    @property
     def cars_rented(self):
-        pass
+        quantity = 0
+        for car in self.purchased_cars:
+            if car.rent:
+                quantity += 1
+        return quantity
 
     #retorna costos de gannacia por los autos rentados
     def calculate_costs_rents(self):
-        pass
+        value = self.cars_rented*self.rent_value
+        return value
 
     #retorna costos de perdidas por autos ociosos
     def calculate_costs_idle(self):
-        pass
+        value = self.cars_idle*self.idle_value
+        return value
 
     #retorna costos de perdidas por no tener autos con se solicitaron
-    def calculate_costs_no_available(self, requested_cars):
-        pass
+    def calculate_costs_no_available(self, requested_cars=None, no_rented=None):
+        if requested_cars != None:
+            availability = requested_cars-self.cars_idle
+            if availability < 0:
+                return abs(availability)*self.busy_value
+        if no_rented != None:
+            return abs(availability)*self.busy_value
+        return 0
 
     #renta carros si esta disponible, retorna carros rentados y no rentados
-    def rent_cars(self, requested_cars):
-        pass
+    def rent_car(self, rent_days):
+        rented = False
+        for car in self.purchased_cars:
+            if !car.rent:
+                car.rent_car(rent_days)
+                rented = True
+                break
+        return rented
 
     #actualiza la renta de los autos
     def update_rents(self):
-        pass
+        for car in self.purchased_cars:
+            if car.rent:
+                car.update_rent_days
